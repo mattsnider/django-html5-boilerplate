c27089d53c885184996f35e8b47615375b6ec1f0
mattsnider <mattesnider@gmail.com>
2013-08-21 18:28:15 -0700

    Adding a second base.html ('base_script_in_head.html') that moves the script blocks to head for use with mobile libraries like jQuery

--- dh5bp/__init__.py
+++ dh5bp/__init__.py
@@ -1,2 +1,2 @@
 __author__ = 'msnider'
-__version__ = (1, 0, 4)
+__version__ = (1, 0, 5)
--- fabfile.py
+++ fabfile.py
@@ -45,8 +45,22 @@ def migrateHtml():
     e404PathDest = os.path.join(templatePath, '404.html')
     e500PathDest = os.path.join(templatePath, '500.html')
     basePathDest = os.path.join(templatePath, 'base.html')
+    baseJsOnTopPathDest = os.path.join(templatePath, 'base_script_in_head.html')
 
     local('cp %s %s' % (e404PathSource, e404PathDest))
+
+    var_body_attr = 'body_attr'
+    var_ga_code = 'DH5BP_GA_CODE'
+    var_html_attr = 'html_attr'
+    var_main_js = 'main_js'
+
+    block_content = 'content'
+    block_description = 'description'
+    block_head = 'head'
+    block_outdated_message = 'outdated_message'
+    block_post_main_script = 'post_main_script'
+    block_pre_main_script = 'pre_main_script'
+    block_title = 'title'
 
     # create a 500 error page
     with open(e404PathSource, 'r') as f:
@@ -80,17 +94,21 @@ def migrateHtml():
 
         # add a attribute template variables to the HTML and BODY tag
         text = text.replace(
-            '<html class', '<html {{ html_attr|safe }} class')
+            '<html class', '<html {{ %s|safe }} class' % var_html_attr)
         text = text.replace(
-            '<body>', '<body {{ body_attr|safe }}>')
+            '<body>', '<body {{ %s|safe }}>' % var_body_attr)
 
         # update the head block (title, meta, additional scripts)
         text = text.replace(
-            '<title></title>', '<title>{% block title %}{% endblock %}</title>')
+            '<title></title>',
+            '<title>{%% block %s %%}{%% endblock %%}</title>' % block_title)
         text = text.replace(
-            'content=""', 'content="{% block description %}{% endblock %}"')
+            'content=""',
+            'content="{%% block %s %%}{%% endblock %%}"' % block_description)
         text = text.replace(
-            '</head>', '    {% block head %}{% endblock %}\n    </head>')
+            '</head>',
+            '    {%% block %s %%}{%% endblock %%}\n'
+            '    </head>' % block_head)
 
 
         # make CSS and JS paths use static
@@ -99,29 +117,57 @@ def migrateHtml():
 
         # setup a GA variable, only include script block in final page,
         #   if the code is set
-        text = text.replace('UA-XXXXX-X', '{{ DH5BP_GA_CODE }}')
-        text = text.replace('<script>\n', '{% if DH5BP_GA_CODE %}<script>\n')
+        text = text.replace('UA-XXXXX-X', '{{ %s }}' % var_ga_code)
+        text = text.replace(
+            '<script>\n', '{%% if %s %%}<script>\n' % var_ga_code)
         text = text.replace(
             '\n        </script>', '\n        </script>{% endif %}')
 
         # create the content block
-        text = text.replace('<p>', '{% block content %}<p>')
+        text = text.replace('<p>', '{%% block %s %%}<p>' % block_content)
         text = text.replace('</p>', '</p>{% endblock %}')
 
         # wrap the legacy browser message, so it can be changed
         text = text.replace(
             '<p class="browsehappy">',
-            '{% block outdated_message %}<p class="browsehappy">')
+            '{%% block %s %%}<p class="browsehappy">' % block_outdated_message)
 
-        # change the path for the main script
+        # cleanup extra lines
+        text = text.replace('\n        \n', '        \n')
+        text = text.replace('\n\n', '\n')
+
+        # grab and remove the bottom script section
+        rx_script_matcher = re.compile(
+            '(<script src="//.*main.js".*?</script>)',
+            flags=re.MULTILINE|re.DOTALL)
+        script_section = re.search(rx_script_matcher, text).group(0)
+        script_in_head = re.sub(rx_script_matcher, '', text)
+        replacement_text = """
+        {%% block %s %%}{%% endblock %%}
+        <script src="{%% if %s %%}{%% static %s %%}{%% else %%}{%% static "js/main.js" %%}{%% endif %%}"></script>
+        {%% block %s %%}{%% endblock %%}""" % (
+            block_pre_main_script, var_main_js, var_main_js,
+            block_post_main_script)
+        text = re.sub(rx_script_matcher, '', text)
+        script_section = '%s%s' % (
+            script_section, replacement_text)
+        script_section = script_section.replace(
+            '<script src="{% static "js/dh5bp/main.js" %}"></script>', '')
         text = text.replace(
-            '<script src="{% static "js/dh5bp/main.js" %}"></script>',
-            '{% block pre_main_script %}{% endblock %}\n'
-            '        <script src="{% if main_js %}{% static main_js %}{% else %}{% static "js/main.js" %}{% endif %}"></script>\n'
-            '        {% block post_main_script %}{% endblock %}')
+            '{% if DH5BP_GA_CODE %}',
+            '%s\n        {%% if DH5BP_GA_CODE %%}' % script_section)
 
+        # create a copy with scripts in the head section
+        script_in_head = script_in_head.replace('</head>', """    %s
+    </head>""" % script_section)
+
+    # write the new text to base.html
     with open(basePathDest, 'w') as f:
         f.write(text)
+
+    # write the new text to base_script_in_head.html
+    with open(baseJsOnTopPathDest, 'w') as f:
+        f.write(script_in_head)
 
     # Other Templates
     local('cp %s %s' % (
